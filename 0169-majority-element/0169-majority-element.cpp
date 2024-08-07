8fac5b51c993546e971af7790ebf3a7030062615
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-07 21:06:20 +0530

    Time: 15 ms (77.25%), Space: 26.8 MB (61.83%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+int majorityElement(vector<int> &num) {
+    int cur = 0;
+    int cnt = 0;
+    for (int x : num) {
+        if (cnt == 0) cur = x, cnt = 1;
+        else if (cur == x) ++cnt;
+        else --cnt;
+    }
+    return cur;
+}
+};
