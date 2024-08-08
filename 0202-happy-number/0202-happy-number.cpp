10ad5e01d52f26f2308523eea38220532ad80119
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-08 19:09:35 +0530

    Time: 0 ms (100%), Space: 6.7 MB (100%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,21 @@
+#include <unordered_set>
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        std::unordered_set<int> seen; // To detect cycles
+
+        while (n != 1 && seen.find(n) == seen.end()) {
+            seen.insert(n);
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            n = sum;
+        }
+
+        return n == 1;
+    }
+};
