fe5221727de7e87efa3d59e095875f9c137cc8bc
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-08 19:47:53 +0530

    Time: 60 ms (5.03%), Space: 86.4 MB (5.01%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& preq) {
+        vector<vector<int>> adj(n, vector<int>(n, 0));
+        for(auto it : preq) adj[it[0]][it[1]] = 1;
+        vector<int> indeg(n, 0);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(adj[i][j] == 1) indeg[j]++;
+            }
+        }
+        queue<int> q;
+        for(int i = 0; i < n; i++) {
+            if(indeg[i] == 0) q.push(i);
+        }
+        int res = 0;
+        while(!q.empty()) {
+            int u = q.front();
+            q.pop();
+            res++;
+            for(int v = 0; v < n; v++) {
+                if(adj[u][v] == 1) {
+                    indeg[v]--;
+                    if(indeg[v] == 0) q.push(v);
+                }
+            }
+        }
+        return res == n;
+    }
+};
