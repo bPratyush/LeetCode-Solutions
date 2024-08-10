a0b728f6b0b16f1d4e036d23b88a1018bdcd7a57
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-10 21:14:13 +0530

    Time: 9 ms (38.87%), Space: 9.1 MB (44.58%) - LeetHub

--- 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.cpp
+++ 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.cpp
@@ -4,15 +4,12 @@ public:
         int i = 0, j = 0, sum = 0, ans = 0, N = s.size();
         for (; j < N; ++j) {
             sum += abs(s[j] - t[j]);
-
             while (sum > maxCost) { 
                 sum -= abs(s[i] - t[i]);
                 ++i;
             }
-
             ans = max(ans, j - i + 1);
         }
-
         return ans;
     }
 };
