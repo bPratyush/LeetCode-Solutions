68e047c364e5b807a40a44bbb729918dab16439d
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-10 19:29:15 +0530

    Time: 2 ms (94.05%), Space: 17.5 MB (14.37%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,32 +1,32 @@
 class Solution {
-void bfs(vector<vector<int>>&image,vector<vector<int>>&vis,int color,int newrow,int newcol){
-    int origcolor=image[newrow][newcol];
-    if(origcolor==color) return;
-    queue<pair<int,int>>q;
-    q.push({newrow,newcol});
-    vis[newrow][newcol]=1;
-    image[newrow][newcol]=color;
-    int delrow[]={-1,0,1,0};
-    int delcol[]={0,1,0,-1};
-    while(!q.empty()){
-        auto[r,c]=q.front();
-        q.pop();
-        for(int i=0;i<4;i++){
-            int nrow=r+delrow[i];
-            int ncol=c+delcol[i];
-            if(nrow>=0&&nrow<image.size()&&ncol>=0&&ncol<image[0].size()&&!vis[nrow][ncol]&&image[nrow][ncol]==origcolor){
-                q.push({nrow,ncol});
-                vis[nrow][ncol]=1;
-                image[nrow][ncol]=color;
+public:
+    void bfs(int n,int m,vector<vector<int>>&image,vector<vector<int>>&vis,int sr,int sc,int col){
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        vector<pair<int,int>> deldir = {{-1,0},{0,1},{0,-1},{1,0}};
+        int origcol=image[sr][sc];
+        if(col==origcol) return;
+        vis[sr][sc]=1;
+        image[sr][sc]=col;
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nr=r+deldir[i].first;
+                int nc=c+deldir[i].second;
+                if(nr>=0&&nr<n&&nc>=0&&nc<m&&!vis[nr][nc]&&image[nr][nc]==origcol){
+                    q.push({nr,nc});
+                    vis[nr][nc]=1;
+                    image[nr][nc]=col;
+                }
             }
         }
     }
-}
-public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-       vector<vector<int>>vis(image.size(),vector<int>(image[0].size(),0));
-       bfs(image,vis,color,sr,sc);
+        int n=image.size(), m=image[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+       bfs(n,m,image,vis,sr,sc,color);
        return image;
     }
 };
-
