05c200640f684e4de767a8476d993550b1bdd558
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-10 15:45:03 +0530

    Time: 12 ms (9.53%), Space: 18.4 MB (71.6%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) { // renamed from 'merge' to 'mergeTwoLists'
+    struct ListNode* merge(struct ListNode* list1, struct ListNode* list2) {
     if(!list1) return list2;
     if(!list2) return list1;
-    ListNode* ptr=new ListNode(-1);
-    ListNode* curr=ptr;
-    while(list1&&list2){
-        if(list1->val<list2->val){
-            curr->next=list1;
-            list1=list1->next;
-        }
-        else{
-            curr->next=list2;
-            list2=list2->next;
-        }
-        curr=curr->next;
+    if(list1->val<list2->val) {
+        list1->next=merge(list1->next,list2);
+        return list1;
     }
-    if(!list1) curr->next=list2;
-    if(!list2) curr->next=list1;
-    return ptr->next;
+    else{
+        list2->next=merge(list1,list2->next);
+        return list2;
+    }
+}
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) { // renamed from 'merge' to 'mergeTwoLists'
+    struct ListNode* tmp = merge(list1,list2);
+    return tmp;
 }
 };
