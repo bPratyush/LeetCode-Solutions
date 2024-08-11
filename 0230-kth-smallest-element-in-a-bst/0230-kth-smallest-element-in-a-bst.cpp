5c9ee7193ddd89a637dacce9166607f1937d2c19
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-11 12:06:54 +0530

    Time: 12 ms (65.05%), Space: 22.9 MB (24%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -1,20 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int kthSmallest(TreeNode* root, int k) {
-        int result = 0, count = 0;
-        kthSmallestRecursive(root, k, count, result);
-        return result;
+    void inorder(TreeNode* root, vector<int>& res) {
+        if (root) {
+            inorder(root->left, res);
+            res.push_back(root->val);
+            inorder(root->right, res);
+        }
     }
 
-private:
-    void kthSmallestRecursive(TreeNode* node, int k, int& count, int& result) {
-        if (node == nullptr) return;
-        kthSmallestRecursive(node->left, k, count, result);
-        count++;
-        if (count == k) {
-            result = node->val;
-            return;
-        }
-        kthSmallestRecursive(node->right, k, count, result);
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> res;
+        inorder(root, res);
+        return res[k - 1];
     }
 };
