8d5e00dc9edae12c47f76b10c2f33eb6dbf92732
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-15 00:37:29 +0530

    Time: 8 ms (28.17%), Space: 15.2 MB (17.71%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -1,38 +1,24 @@
 class Solution{
 public:
-    void copylist(Node* head){
-        Node* tmp=head;
-        Node* n=head->next;
-        while(tmp){
-            Node* copy=new Node(tmp->val);
-            tmp->next=copy;
-            copy->next=n;
-            tmp=n;
-            if(n!=NULL) n=n->next;
-        }
-    }
-    void handlerandom(Node* head){
-        Node* tmp=head;
-        while(tmp){
-            if(tmp->random) tmp->next->random=tmp->random->next;
-            tmp=tmp->next->next;
-        }
-    }
-    Node* detach(Node* head){
-        Node* dummy=new Node(-1);
-        Node* tail=dummy,*tmp=head;
-        while(tmp){
-            tail->next=tmp->next;
-            tail=tail->next;
-            tmp->next=tail->next;
-            tmp=tmp->next;
-        }
-        return dummy->next;
-    }
     Node* copyRandomList (Node* head){
-        if(!head) return NULL;
-        copylist(head);
-        handlerandom(head);
-        return detach(head);
+        map<Node*, Node*> oldToCopy;
+        oldToCopy[NULL] = NULL;
+
+        Node* cur = head;
+        while (cur != NULL) {
+            Node* copy = new Node(cur->val);
+            oldToCopy[cur] = copy;
+            cur = cur->next;
+        }
+
+        cur = head;
+        while (cur != NULL) {
+            Node* copy = oldToCopy[cur];
+            copy->next = oldToCopy[cur->next];
+            copy->random = oldToCopy[cur->random];
+            cur = cur->next;
+        }
+
+        return oldToCopy[head];
     }
 };
