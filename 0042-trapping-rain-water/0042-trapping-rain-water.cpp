04066fe797ab5a90e34cebf29f6f91b6450db18c
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-14 23:36:26 +0530

    Time: 12 ms (42.97%), Space: 24.8 MB (11.68%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int trap(vector<int>& h) {
-        int l=0,r=h.size()-1,lmax=INT_MIN,rmax=INT_MIN,ans=0;
+        int l=0,r=h.size()-1,lh=INT_MIN,rh=INT_MIN,ans=0;
         while(l<r){
-            lmax=max(lmax,h[l]);
-            rmax=max(rmax,h[r]);
-            ans+=(lmax<rmax)?lmax-h[l++]:rmax-h[r--];
+            lh=max(lh,h[l]);
+            rh=max(rh,h[r]);
+            ans+=(lh<rh)?(lh-h[l++]):(rh-h[r--]);
         }
         return ans;
     }
