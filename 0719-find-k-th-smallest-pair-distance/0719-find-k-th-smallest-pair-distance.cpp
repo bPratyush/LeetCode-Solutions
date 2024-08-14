8932bfe3ef935216e8232e8e47a3f39e7dd6b43d
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-14 12:24:23 +0530

    Time: 12 ms (47.74%), Space: 12.9 MB (44.69%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int smallestDistancePair(vector<int>& nums, int k) {
-        if(k==25000000 && nums[0]==2) return 3;
-        if(k==25000000 && nums[0]==197180) return 292051;
-        if(k==25000000) return 1;
-        int n=nums.size();
-        priority_queue<int> p;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                p.push(abs(nums[i]-nums[j]));
-                if(p.size()>k)p.pop();
-            }
+    bool isPossible(vector<int> &nums, int diff, int k){
+        int pairs=0, j=0;
+        for(int i=0;i<nums.size();i++){
+            while(j<nums.size() && nums[j]-nums[i]<=diff) j++;
+            pairs+=(j-i-1);
         }
-        return p.top();
+        return pairs>=k;
+    }
+    
+    int smallestDistancePair(vector<int>& nums, int k) {
+        int low=0, high=1000000;
+        sort(nums.begin(),nums.end());
+        while(low<high){
+            int mid=(low+high)/2;
+            if(isPossible(nums,mid,k)==true) high=mid;
+            else low=mid+1;
+        }
+        return low;
     }
 };
