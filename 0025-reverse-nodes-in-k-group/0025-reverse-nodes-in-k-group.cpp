fc21773e3e414a163409a9a27b45a30335616dd7
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-15 00:51:42 +0530

    Time: 12 ms (35.92%), Space: 16.5 MB (32.49%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,29 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    void reverse(ListNode* start, ListNode* end) {
-        ListNode* curr = start;
-        ListNode* prev = NULL;
-        while (curr != end) {
-            ListNode* fwd = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = fwd;
-        }
+   ListNode* reverse(ListNode* head, ListNode* tail) {
+    ListNode* curr = head;
+    ListNode* prev = NULL;
+    
+    while (curr != tail) {
+        ListNode* fwd = curr->next;
         curr->next = prev;
+        prev = curr;
+        curr = fwd;
     }
+    
+    return prev; // Return the new head after reversal
+}
 
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        if (!head || k == 1) return head;
-        ListNode* s = head;
-        ListNode* e = head;
-        int inc = k - 1;
-        while (inc--) {
-            e = e->next;
-            if (!e) return head;
-        }
-        ListNode* nextHead = reverseKGroup(e->next, k);
-        reverse(s, e);
-        s->next = nextHead;
-        return e;
+ListNode* reverseKGroup(ListNode* head, int k) {
+    ListNode* tmp = head;
+    for (int i = 0; i < k; ++i) {
+        if (!tmp) return head;
+        tmp = tmp->next;
     }
+    ListNode* newHead = reverse(head, tmp);
+    head->next = reverseKGroup(tmp, k);
+    return newHead;
+}
 };
