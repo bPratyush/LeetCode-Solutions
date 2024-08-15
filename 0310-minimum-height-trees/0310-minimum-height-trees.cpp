b6816f84baa3e223e7df171bb7133059ee0ee16f
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-15 14:51:12 +0530

    Time: 124 ms (37.31%), Space: 60.2 MB (41.84%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if (n==1) return {0}; // edge case
-        vector<int> deg(n, 0); // degree
+        if(n==1) return {0};
+        vector<int> deg(n,0);
         vector<vector<int>> adj(n);
-        for (auto& e : edges) {
-            int v = e[0], w = e[1];
+        for(auto e:edges){
+            int v=e[0],w=e[1];
             adj[v].push_back(w);
             adj[w].push_back(v);
-            deg[v]++;
-            deg[w]++;
+            deg[v]++; deg[w]++;
         }
-        queue<int> q; 
-        for (int i = 0; i < n; i++) {
-            if (deg[i] == 1)
-                q.push(i);
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(deg[i]==1) q.push(i);
         }
         vector<int> ans;
-        int left=n;
-        while (!q.empty()) {
-            int qz = q.size();
-            left-=qz; // how many left
-            for (int i = 0; i < qz; i++) {
-                int v = q.front();
+        int lft=n;
+        while(!q.empty()){
+            int qz=q.size();
+            lft-=qz;
+            for(int i=0;i<qz;i++){
+                int v=q.front();
                 q.pop();
-                if (left==0)//centers are found
-                    ans.push_back(v);
-                for (int w : adj[v]) {
-                    if (--deg[w] == 1)// remove edge connecting to v
-                        q.push(w);
+                if(lft==0) ans.push_back(v);
+                for(int w:adj[v]){
+                    if(--deg[w]==1) q.push(w);
                 }
             }
         }
