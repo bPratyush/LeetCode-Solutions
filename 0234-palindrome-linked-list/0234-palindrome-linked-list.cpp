90f59d89e14725dbdc38a26059b682492291de4f
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-15 18:19:47 +0530

    Time: 149 ms (87.79%), Space: 116.5 MB (91.37%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,42 +10,32 @@
  */
 class Solution {
 public:
-    int getLength(ListNode* head) {
-    int n = 0;
-    ListNode* tmp = head;
-    while (tmp) {
-        n++;
-        tmp = tmp->next;
-    }
-    return n;
-}
-
-ListNode* reverse(ListNode* head) {
-    ListNode* curr = head;
-    ListNode* prev = NULL;
-    while (curr) {
-        ListNode* fwd = curr->next;
+    void reverse(struct ListNode** head_ref){
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = *head_ref;
+    struct ListNode* next = NULL;
+    while(curr) {
+        next = curr->next;
         curr->next = prev;
         prev = curr;
-        curr = fwd;
+        curr = next;
     }
-    return prev;
+    *head_ref = prev;
 }
 
-bool isPalindrome(ListNode* head) {
-    if (!head || !head->next) return true;
-    int n=getLength(head);
-    ListNode* tmp = head;
-    for (int i = 0; i < n / 2; i++) {
-        tmp = tmp->next;
+bool isPalindrome(struct ListNode* head) {
+    if(!head || !head->next) return true;
+    struct ListNode *slow = head, *fast = head;
+    while(fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
     }
-    ListNode* tmp1 = reverse(tmp);
-    ListNode* ptr1 = head;
-    ListNode* ptr2 = tmp1;
-    while (ptr2) {
-        if (ptr1->val != ptr2->val) return false;
-        ptr1 = ptr1->next;
-        ptr2 = ptr2->next;
+    reverse(&slow);
+    struct ListNode* curr = head;
+    while(slow) {
+        if(curr->val != slow->val) return false;
+        curr = curr->next;
+        slow = slow->next;
     }
     return true;
 }
