6879dcd001f354935c7b92726ff84a8b0342c353
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-15 08:11:42 +0530

    Time: 74 ms (48.46%), Space: 86.1 MB (32.71%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,5 +1,3 @@
-#include <vector>
-
 class Solution {
 public:
     bool lemonadeChange(std::vector<int>& bills) {
@@ -7,22 +5,17 @@ public:
         int count10 = 0;
 
         for (int bill : bills) {
-            if (bill == 5) {
-                count5++;
-            } else if (bill == 10) {
+            if (bill == 5) count5++;
+            else if (bill == 10) {
                 count10++;
                 count5--;
             } else if(bill == 20){
                 if (count10 > 0) {
                     count10--;
                     count5--;
-                } else {
-                    count5 -= 3;
-                }
+                } else count5 -= 3;
             }
-            if (count5 < 0) {
-                return false;
-            }
+            if (count5 < 0) return false;
         }
         return true;
     }
