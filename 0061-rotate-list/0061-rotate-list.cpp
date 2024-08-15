44d64db44b605c7790e5230187996641822f5023
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-15 08:42:43 +0530

    Time: 3 ms (84.93%), Space: 16.7 MB (23.53%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -10,38 +10,20 @@
  */
 class Solution {
 public:
-    vector<int> LLtoarr(ListNode* head){
-        vector<int> res;
-        ListNode* tmp=head;
-        while(tmp){
-            res.push_back(tmp->val);
-            tmp=tmp->next;
-        }
-        return res;
-    }
-    vector<int> rotateright(vector<int>&arr,int k){
-        if(arr.size()==0) return {};
-        int n=arr.size();
-        k%=n;
-        vector<int> narr(n);
-        for(int i=0;i<n;i++){
-            narr[(i+k)%n]=arr[i];
-        }
-        return narr;
-    }
-    ListNode* arrtoLL(vector<int> res){
-        if (res.empty()) return nullptr; 
-    ListNode* head = new ListNode(res[0]); 
-    ListNode* current = head; 
-    for (int i = 1; i < res.size(); i++) {
-        current->next = new ListNode(res[i]);  
-        current = current->next; 
-    }
-    return head;
-    }
     ListNode* rotateRight(ListNode* head, int k) {
-      vector<int>arr=LLtoarr(head);
-      vector<int>res=rotateright(arr,k);
-      return arrtoLL(res);
+      if(head == NULL||head->next == NULL||k == 0) return head;
+        ListNode* temp = head;
+    int length = 1;
+    while(temp->next != NULL) {
+        ++length;
+        temp = temp->next;
+    }
+    temp->next = head;
+    k = k%length; 
+    int end = length-k;
+    while(end--) temp = temp->next;
+    head = temp->next;
+    temp->next = NULL;
+    return head;
     }
 };
