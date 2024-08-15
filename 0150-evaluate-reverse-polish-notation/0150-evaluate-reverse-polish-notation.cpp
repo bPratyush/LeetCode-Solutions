46a99f92c9a725d961d48fe1a632228e672f7d61
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-15 14:28:47 +0530

    Time: 3 ms (96.85%), Space: 15.9 MB (16.21%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -10,14 +10,12 @@ public:
         };
 
         for (auto str : tokens) {
-            if (m.find(str) == m.end()) {
-                stck.push(stoi(str));
-            } else {
+            if (m.find(str) == m.end()) stck.push(stoi(str));
+            else {
                 int s1 = stck.top();
                 stck.pop();
                 int s2 = stck.top();
                 stck.pop();
-
                 stck.push(m[str](s2, s1));
             }
         }
