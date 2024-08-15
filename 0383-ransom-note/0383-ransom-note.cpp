17e1b1a1f8bf108ff9ebfc1db873052f66e59853
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-15 13:34:21 +0530

    Time: 12 ms (77.77%), Space: 11.5 MB (33.84%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        vector<int> alphabet(26, 0);
-        for (char c : ransomNote) {
-            size_t i = magazine.find(c, alphabet[c - 'a']);
-            if (i == string::npos) return false;
-            alphabet[c - 'a'] = i + 1;
+        unordered_map<char , int>count;
+        for(int i = 0  ; i < magazine.length() ; i++) count[magazine[i]]++;
+        for (int i = 0; i < ransomNote.length(); i++) {
+            count[ransomNote[i]]--;
+            if (count[ransomNote[i]] < 0) return false;
         }
         return true;
     }
