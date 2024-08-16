a1ca070a1caf2e421a5e11c1a6d5bb197cac70ba
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-16 11:08:29 +0530

    Time: 231 ms (59.91%), Space: 107.9 MB (57.55%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -4,14 +4,9 @@ public:
     int min_value = arrays[0].front();
         int max_value = arrays[0].back();
         int max_dist = 0;
-        
-        // Iterate through the rest of the arrays
         for (int i = 1; i < arrays.size(); ++i) {
-            // Calculate the distance using the current array's elements and global min/max
             max_dist = max(max_dist, abs(arrays[i].back() - min_value));
             max_dist = max(max_dist, abs(max_value - arrays[i].front()));
-            
-            // Update global min and max values
             min_value = min(min_value, arrays[i].front());
             max_value = max(max_value, arrays[i].back());
         }
