a8fa4a964bfd6b60c0ce9a908f84cae4a11f4473
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-16 19:40:58 +0530

    Time: 3 ms (72.88%), Space: 14.3 MB (69.49%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,25 +12,31 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>> nodes;
-        queue<pair<TreeNode*,pair<int,int>>> todo;
-        todo.push({root,{0,0}});
-        while(!todo.empty()){
-            auto p=todo.front();
-            todo.pop();
-            TreeNode* node = p.first;
-            int x=p.second.first;
-            int y=p.second.second;
+        map<int, map<int, multiset<int>>> nodes;
+        queue<pair<TreeNode*,pair<int,int>>>q;
+        q.push({root,{0,0}});
+        while(!q.empty()){
+            auto u=q.front();
+            q.pop();
+            TreeNode* node=u.first;
+            int x=u.second.first;
+            int y=u.second.second;
             nodes[x][y].insert(node->val);
-            if(node->left) todo.push({node->left,{x-1,y+1}});
-            if(node->right) todo.push({node->right,{x+1,y+1}});
+            if(node->left) q.push({node->left,{x-1,y+1}});
+            if(node->right) q.push({node->right,{x+1,y+1}});
         }
         vector<vector<int>> res;
-        for(auto p:nodes){
-            vector<int> col;
-            for(auto q:p.second) col.insert(col.end(),q.second.begin(),q.second.end());
-            res.push_back(col);
-        }
-        return res;
-    }   
+    // Iterate over the outer map
+    for (const auto& p : nodes) {
+    vector<int> col;
+    // Iterate over the inner map
+    for (const auto& q : p.second) {
+        // Insert elements from the multiset into the column vector
+        col.insert(col.end(), q.second.begin(), q.second.end());
+    }
+    // Add the column vector to the result
+    res.push_back(col);
+}
+return res;
+    }
 };
