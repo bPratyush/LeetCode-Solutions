9b6d07ba93286a1b87117b6ebbc088ae7eab792e
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-16 11:53:57 +0530

    Time: 971 ms (5.03%), Space: 275.6 MB (5.12%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int> dist(n, INT_MAX);
-        dist[k - 1] = 0;
-        for (int i = 1; i < n; ++i) {
-            for (auto e : times) {
-                int u = e[0], v = e[1], w = e[2];
-                if (dist[u - 1] == INT_MAX) continue;
-                dist[v - 1] = min(dist[v - 1], dist[u - 1] + w);
+        vector<int> dist(n,INT_MAX);
+        dist[k-1]=0;
+        for(int i=1;i<n;i++){
+            for(auto it:times){
+            int u=it[0],v=it[1],w=it[2];
+            if(dist[u-1]==INT_MAX) continue;
+            dist[v-1]=min(dist[v-1],dist[u-1]+w);
             }
         }
-        int maxDelay = *max_element(dist.begin(), dist.end());
-        return (maxDelay == INT_MAX) ? -1 : maxDelay;
+    int maxdel=*max_element(dist.begin(),dist.end());
+    return (maxdel==INT_MAX)?-1:maxdel;
     }
 };
