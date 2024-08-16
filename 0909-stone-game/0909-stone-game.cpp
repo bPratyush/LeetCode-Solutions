70ddf6c43afa5e13e15bc16158e1b74643a9bdca
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-16 16:51:42 +0530

    Time: 0 ms (100%), Space: 9.4 MB (72.09%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-       return true; 
+        int A=0,B=0;
+        sort(piles.begin(),piles.end());
+        int n=piles.size();
+        int alice=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(alice)   A+=piles[i];
+            else    B+=piles[i];
+            alice=!alice;
+        }
+
+        return A>B;
     }
 };
