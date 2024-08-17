4cd50658d979e3874b4331f6f6bc652d1a919492
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-17 17:20:08 +0530

    Time: 73 ms (5.29%), Space: 86.7 MB (5.24%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& preq) {
-    vector<vector<int>> adj(n,vector<int>(n, 0));
-    vector<int> indeg(n, 0);
-        for (auto it : preq) {
-            int u = it[1], v = it[0];
-            if (adj[u][v] == 0) {  // Ensure we don't count the same edge twice
-                adj[u][v] = 1;
-                indeg[v]++;
+        vector<vector<int>> adj(n,vector<int>(n,0));
+        vector<int> indeg(n,0);
+        for(auto it:preq) adj[it[0]][it[1]]=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(adj[i][j]==1) indeg[j]++;
             }
         }
-        queue<int> q;
-        for (int i = 0; i < n; ++i) {
-            if (indeg[i] == 0) q.push(i);
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(!indeg[i]) q.push(i);
         }
         vector<int> res;
-        while (!q.empty()) {
-            int u = q.front();
+        while(!q.empty()){
+            int u=q.front();
             q.pop();
             res.push_back(u);
-            for (int v = 0; v < n; ++v) {
-                if (adj[u][v] == 1) {
+            for(int v=0;v<n;v++){
+                if(adj[u][v]==1){
                     indeg[v]--;
-                    if (indeg[v] == 0) q.push(v);
+                    if(!indeg[v]) q.push(v);
                 }
             }
         }
-        if (res.size() != n) return {};
-        return res;
+        if(res.size()==n) {
+            reverse(res.begin(),res.end());
+            return res;
+        }
+        return {};
     }
 };
