8835438b02a5be0d0078c9629cef334d462b5db6
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-17 12:34:59 +0530

    Time: 127 ms (33.5%), Space: 84.2 MB (46.64%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& A) {
-        A.push_back(0);
-        int n=A.size(),res=0;
-        stack<int>s;
-        for(int i=0;i<n;i++){
-            while(s.size()&&A[i]<=A[s.top()]){
-                int height=A[s.top()];
-                s.pop();
-                int left=s.size()?s.top():-1;
-                res=max(res,(i-left-1)*height);
-            }
-            s.push(i);
+    vector<int> NSE(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> nse(n, n);
+        stack<int> st;
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop();
+            if(!st.empty()) nse[i] = st.top();
+            st.push(i);
         }
-        return res;
+        return nse;
+    }
+
+    vector<int> PSE(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> pse(n, -1);
+        stack<int> st;
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && heights[st.top()] >= heights[i]) st.pop();
+            if(!st.empty()) pse[i] = st.top();
+            st.push(i);
+        }
+        return pse;
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> nse = NSE(heights), pse = PSE(heights);
+        int maxArea = 0;
+        for(int i = 0; i < n; i++) {
+            int width = nse[i] - pse[i] - 1; // Width of the largest rectangle with heights[i] as the smallest height
+            int area = width * heights[i];
+            maxArea = max(maxArea, area);
+        }
+        return maxArea;
     }
 };
