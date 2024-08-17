379ff1f8b06657b18b93c9bd7abca0bc1b8d7fb2
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-17 12:26:16 +0530

    Time: 8 ms (74.08%), Space: 20.7 MB (59.98%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
-        vector<int> asteroidCollision(std::vector<int>& asteroids) {
-        int n = asteroids.size(), j = 0;
-        for (int i = 0; i < n; i++) {
-            int asteroid = asteroids[i];
-            while (j > 0 && asteroids[j - 1] > 0 && asteroid < 0 && asteroids[j - 1] < abs(asteroid)) j--;
-            if (j == 0 || asteroid > 0 || asteroids[j - 1] < 0) asteroids[j++] = asteroid;
-            else if (asteroids[j - 1] == abs(asteroid)) j--;
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> Stack;
+        for(int asteroid: asteroids) {
+            bool destroyed = false;
+            while(!Stack.empty() && asteroid < 0 && Stack.top() > 0) {
+               if(-asteroid == Stack.top()) {
+                   destroyed = true;
+                   Stack.pop();
+                   break;
+               }
+               else if(-asteroid > Stack.top()) {
+                   Stack.pop();
+               }
+               else {
+                   destroyed = true;
+                   break;
+               }
+           }
+            if(!destroyed)
+            Stack.push(asteroid);
         }
-        asteroids.resize(j);
-        return asteroids;
+        vector<int> result(Stack.size());
+        for(int i = Stack.size() - 1; i >= 0; i--) {
+            result[i] = Stack.top();
+            Stack.pop();
+        }
+        return result;
     }
 };
-
-
