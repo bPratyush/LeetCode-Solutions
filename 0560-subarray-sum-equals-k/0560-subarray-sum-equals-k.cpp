2d59279db161b2e73b26dc41a588ad204ea86034
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-19 16:38:54 +0530

    Time: 48 ms (98.58%), Space: 45.4 MB (47.8%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -6,7 +6,7 @@ public:
         int cursum=0,cnt=0;
         for(int i=0;i<nums.size();i++){
             cursum+=nums[i];
-            cnt+=hash[cursum-k];
+            if(hash.find(cursum-k)!=hash.end())cnt+=hash[cursum-k];
             hash[cursum]++;
         }
         return cnt;
