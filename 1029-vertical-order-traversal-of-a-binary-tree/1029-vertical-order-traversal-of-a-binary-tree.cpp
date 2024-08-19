3f0e141398f80cb0f02680d598230c6d1ed53edd
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2024-08-19 15:29:04 +0530

    Time: 0 ms (100%), Space: 15 MB (35.13%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,31 +12,27 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> nodes;
-        queue<pair<TreeNode*,pair<int,int>>>q;
+        map<int,map<int,multiset<int>>> nodes;
+        queue<pair<TreeNode*,pair<int,int>>> q;
         q.push({root,{0,0}});
         while(!q.empty()){
-            auto u=q.front();
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+            TreeNode* node=q.front().first;
+            int x=q.front().second.first;
+            int y=q.front().second.second;
             q.pop();
-            TreeNode* node=u.first;
-            int x=u.second.first;
-            int y=u.second.second;
             nodes[x][y].insert(node->val);
             if(node->left) q.push({node->left,{x-1,y+1}});
             if(node->right) q.push({node->right,{x+1,y+1}});
+            }
         }
-        vector<vector<int>> res;
-    // Iterate over the outer map
-    for (const auto& p : nodes) {
-    vector<int> col;
-    // Iterate over the inner map
-    for (const auto& q : p.second) {
-        // Insert elements from the multiset into the column vector
-        col.insert(col.end(), q.second.begin(), q.second.end());
-    }
-    // Add the column vector to the result
-    res.push_back(col);
-}
-return res;
+        vector<vector<int>>res;
+        for(auto u:nodes){
+            vector<int>col;
+            for(auto q:u.second) col.insert(col.end(),q.second.begin(),q.second.end());
+            res.push_back(col);
+        }
+        return res;
     }
 };
