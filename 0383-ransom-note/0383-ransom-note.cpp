128363eb58194eac71a52d67b83cbb9237480e99
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-01 21:25:46 +0530

    Time: 0 ms (100%), Space: 12 MB (29.91%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char , int>count;
-        for(int i = 0  ; i < magazine.length() ; i++) count[magazine[i]]++;
-        for (int i = 0; i < ransomNote.length(); i++) {
-            count[ransomNote[i]]--;
-            if (count[ransomNote[i]] < 0) return false;
+        unordered_map<char,int> mpp;
+        for(int i=0;i<magazine.length();i++) mpp[magazine[i]]++;
+        for(int i=0;i<ransomNote.length();i++){
+            mpp[ransomNote[i]]--;
+            if(mpp[ransomNote[i]]<0) return false;
         }
         return true;
     }
