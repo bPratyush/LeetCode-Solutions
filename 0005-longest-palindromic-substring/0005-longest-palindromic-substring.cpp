910331b0c77ec11731a3bf1561dbee6459d55cf4
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-03 23:09:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-bool isPalindrome(const std::string& str, int l, int r) {
-    while (l < r) {
-        if (str[l++] != str[r--]) return false;
+    bool ispal(string s,int l,int r){
+        while(l<=r){
+        if(s[l++]!=s[r--]) return false;
+        }
+        return true;
     }
-    return true;
-}
     string longestPalindrome(string s) {
-        int n = s.length();
-        if (n == 0) return "";
-        int maxLength = 0, start = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (isPalindrome(s, i, j) && (j - i + 1) > maxLength) {
-                    maxLength = j - i + 1;
-                    start = i;
+        int n=s.length();
+        if(!n) return "";
+        int maxlen=0,start=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(ispal(s,i,j)&&(j-i+1)>maxlen){
+                    maxlen=j-i+1;
+                    start=i;
+                }
             }
         }
-    }
-    return s.substr(start, maxLength);
+        return s.substr(start,maxlen);
     }
 };
