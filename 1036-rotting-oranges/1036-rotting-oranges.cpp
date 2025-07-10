a373cb18b8dd5662605dc66b02172eb08210db7c
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-10 21:20:40 +0530

    Time: 4 ms (17.44%), Space: 17.6 MB (11.37%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
        queue<pair<pair<int,int>,int>>q;
-       int n=grid.size(), m=grid[0].size(), t=0;
+       int n=grid.size(),m=grid[0].size(),t=0;
        vector<vector<int>> vis(n,vector<int>(m,0));
-       vector<vector<int>> dr={{-1,0},{0,1},{1,0},{0,-1}};
+       vector<vector<int>> del={{-1,0},{0,1},{1,0},{0,-1}};
        for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             if(grid[i][j]==2){
@@ -12,29 +12,27 @@ public:
                 vis[i][j]=2;
             }
         }
-       }
-       while(!q.empty()){
+    }
+    while(!q.empty()){
         int r=q.front().first.first;
         int c=q.front().first.second;
-        int ct=q.front().second;
+        int tm=q.front().second;
         q.pop();
-        t=max(t,ct);
+        t=max(t,tm);
         for(int i=0;i<4;i++){
-            int nr=r+dr[i][0];
-            int nc=c+dr[i][1];
+            int nr=r+del[i][0];
+            int nc=c+del[i][1];
             if(nr>=0&&nr<n&&nc>=0&&nc<m&&vis[nr][nc]!=2&&grid[nr][nc]==1){
-                q.push({{nr,nc},ct+1});
+                q.push({{nr,nc},tm+1});
                 vis[nr][nc]=2;
             }
         }
-       }
-       for(int i=0;i<n;i++){
+    }
+    for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            if(vis[i][j]!=2&&grid[i][j]==1){
-                return -1;
-            }
+            if(vis[i][j]!=2&&grid[i][j]==1) return -1;
         }
-       }
-       return t;
+    }
+    return t;
     }
 };
