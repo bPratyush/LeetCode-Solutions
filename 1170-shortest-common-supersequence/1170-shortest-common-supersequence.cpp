92877313a6b0713148f0813a39fb7d61e560b2f3
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-11 22:20:40 +0530

    Time: 23 ms (88.98%), Space: 26.8 MB (71.46%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -1,46 +1,41 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string text1, string text2) {
-        //Longest Common Subsequence DP Top Down Approach
-        int n=text1.length(), m=text2.length();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0)); 
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n=str1.length(), m=str2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-            if(text1[i-1]==text2[j-1]) dp[i][j]=max(dp[i][j],1+dp[i-1][j-1]);
-            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                if(str1[i-1]==str2[j-1]) dp[i][j]=max(dp[i][j],dp[i-1][j-1]+1);
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        //Printing LCS
         int i=n,j=m;
-        string LCS="";
-        while(i>0 && j>0){
-            if(text1[i-1]==text2[j-1]){
-                LCS+=text1[i-1];
-                i--;
-                j--;
+        string res="";
+        while(i>0&&j>0){
+            if(str1[i-1]==str2[j-1]){
+                res+=str1[i-1];
+                i--; j--;
             }
             else{
                 if(dp[i-1][j]>dp[i][j-1]){
-                    LCS+=text1[i-1];
+                    res+=str1[i-1];
                     i--;
                 }
                 else{
-                    LCS+=text2[j-1];
+                    res+=str2[j-1];
                     j--;
                 }
             }
         }
-        //Taking remaining characters from str1 and str2
         while(i>0){
-            LCS+=text1[i-1];
+            res+=str1[i-1];
             i--;
         }
         while(j>0){
-            LCS+=text2[j-1];
+            res+=str2[j-1];
             j--;
         }
-        //Reversal not mandatory here but used for printing LCS
-        reverse(LCS.begin(),LCS.end());
-        return LCS;
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
