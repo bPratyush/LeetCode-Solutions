01ed24be3ae303de25dd22f74ab04090e69d7083
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-12 15:42:16 +0530

    Time: 68 ms (5.42%), Space: 87.5 MB (5.07%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& preq) {
-        vector<vector<int>> adj(n,vector<int>(n,0));
-        vector<int> indeg(n,0);
+        vector<vector<int>>adj(n,vector<int>(n,0));
+        vector<int>indeg(n,0);
         for(auto it:preq) adj[it[0]][it[1]]=1;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
@@ -13,21 +13,20 @@ public:
         for(int i=0;i<n;i++){
             if(!indeg[i]) q.push(i);
         }
-        vector<int> res;
+        vector<int>res;
         while(!q.empty()){
             int u=q.front();
             q.pop();
             res.push_back(u);
-            for(int v=0;v<n;v++){
-                if(adj[u][v]==1){
-                    indeg[v]--;
-                    if(!indeg[v]) q.push(v);
+            for(int i=0;i<n;i++){
+                if(adj[u][i]==1){
+                    indeg[i]--;
+                    if(!indeg[i]) q.push(i);
                 }
             }
         }
-        if(res.size()==n) {
-            reverse(res.begin(),res.end());
-            return res;
+        if(res.size()==n){
+            reverse(res.begin(),res.end()); return res;
         }
         return {};
     }
