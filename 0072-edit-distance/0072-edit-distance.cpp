ff988dfe12d6784d7f7c57ce0225d9a0c2c465b8
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-12 20:51:21 +0530

    Time: 7 ms (59.39%), Space: 13.4 MB (27.47%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -9,8 +9,8 @@ public:
         for(int j=1;j<=m;j++){
             if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1];
             else dp[i][j]=min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
+            }
         }
-       }
-       return dp[n][m];
+        return dp[n][m];
     }
 };
