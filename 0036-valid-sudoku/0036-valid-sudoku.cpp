b08c387c0afca674f7fce71d34219eca17fa865e
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-12 19:50:29 +0530

    Time: 3 ms (63.04%), Space: 23.4 MB (59.27%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<char,int> eachBox;
-        unordered_map<char,int> row;
-        unordered_map<char,int> column ;
-        for(int i =0 ; i < 9 ;i++){
-            for(int j=0 ; j< 9 ;j++){
-                if(board[i][j] != '.') row[board[i][j]]++;
-                if(board[j][i]!='.') column[board[j][i]]++;
-                if(row[board[i][j]]>1) return false;
-                if(column[board[j][i]]>1) return false ;
+        unordered_map<char,int> box,row,col;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]!='.') row[board[i][j]]++;
+                if(board[j][i]!='.') col[board[j][i]]++;
+                if(row[board[i][j]]>1 || col[board[j][i]]>1) return false;
             }
-            row.clear();
-            column.clear();
+            row.clear(); col.clear();
         }
-        for(int i=0 ; i < 9 ;i+=3){
-            for(int j=0 ; j <9 ;j+=3){
-                eachBox.clear();
-                for(int x = i ; x < i+3 ; x++){
-                    for(int y = j ; y < j+3; y++){
-                        if(board[x][y]!='.') eachBox[board[x][y]]++;
-                        if(eachBox[board[x][y]]>1) return false ;
+        for(int i=0;i<9;i+=3){
+            for(int j=0;j<9;j+=3){
+                box.clear();
+                for(int x=i;x<i+3;x++){
+                    for(int y=j;y<j+3;y++){
+                        if(board[x][y]!='.') box[board[x][y]]++;
+                        if(box[board[x][y]]>1) return false;
                     }
                 }
             }
-        }
+        }  
         return true;
     }
 };
