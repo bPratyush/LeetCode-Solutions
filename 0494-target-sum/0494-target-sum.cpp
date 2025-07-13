2ea65de4363e0ef139404f071d2e3df8705f4fa8
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-13 09:47:50 +0530

    Time: 8 ms (63.63%), Space: 15.7 MB (40.24%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
-        for(int x: nums) sum += x;
-        if (abs(target) > sum || (sum + target) % 2 != 0)return 0;
-        int s1 = (sum + target)/2;
-        int ans = subsetCount(nums,s1);
-        return ans;
-    }
-    
-    int subsetCount(vector<int>& nums, int s1){
-        int n = nums.size();
-        int t[n+1][s1+1];
-        t[0][0] = 1;
-        for(int i=1;i<s1+1;i++) t[0][i] = 0;
-        for(int i=1;i<n+1;i++)
-            for(int j=0;j<s1+1;j++){
-                if(nums[i-1] > j) t[i][j] = t[i-1][j];
-                else t[i][j] = t[i-1][j] + t[i-1][j-nums[i-1]];
+    int subsetcnt(vector<int>&nums,int s){
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(s+1,0));
+        for(int i=0;i<=s;i++) dp[0][i]=0;
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=s;j++){
+                if(nums[i-1]<=j) dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
+                else dp[i][j]=dp[i-1][j];
             }
-        return t[n][s1];
+        }
+        return dp[n][s];
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum=0;
+        for(int num:nums) sum+=num;
+        if(abs(target)>sum||(sum+target)%2) return 0;
+        int s=(sum+target)/2;
+        return subsetcnt(nums,s);
     }
 };
-
