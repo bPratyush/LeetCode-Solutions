f77e26f58e521b17f335a3ad34ff43487c0d00db
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-14 11:47:19 +0530

    Time: 109 ms (5.63%), Space: 136.9 MB (67.19%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.cpp
@@ -6,17 +6,20 @@ public:
     }
     
     void set(string key, string value, int timestamp) {
-        hash[key].push_back({ timestamp, value});
+        hash[key].push_back({timestamp,value});
     }
     
     string get(string key, int timestamp) {
         if(hash.find(key) == hash.end()) return "";
         if(timestamp < hash[key][0].first) return "";
         string ans = "";
-        int low = 0, hi = hash[key].size()-1;
+        int low=0,hi=hash[key].size()-1;
         while(low <= hi){
             int mid = low + (hi-low)/2;
-            if(hash[key][mid].first <= timestamp) ans = hash[key][mid].second, low = mid+1;
+            if(hash[key][mid].first <= timestamp) {
+                ans = hash[key][mid].second;
+                low = mid+1;
+            }
             else hi = mid-1;
         }
         return ans;
