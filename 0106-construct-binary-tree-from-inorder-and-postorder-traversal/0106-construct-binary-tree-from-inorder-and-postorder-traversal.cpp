26b0540ef896a46d95a58e65d8f15bc901d086ee
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-15 16:35:23 +0530

    Time: 3 ms (71.25%), Space: 27.6 MB (38.2%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,20 +11,20 @@
  */
 class Solution {
 public:
-    TreeNode* PostIn(vector<int>& inorder, int is, int ie, vector<int>& postorder, int ps, int pe, map<int,int>& hm) {
-        if (ps > pe || is > ie) return NULL;
-        TreeNode* root = new TreeNode(postorder[pe]);
-        int inRoot = hm[postorder[pe]];
-        int numsLeft = inRoot - is;
-        root->left = PostIn(inorder, is, inRoot - 1, postorder, ps, ps + numsLeft - 1, hm);
-        root->right = PostIn(inorder, inRoot + 1, ie, postorder, ps + numsLeft, pe - 1, hm);
+    TreeNode* build(vector<int>&inorder,int instart,int inend,vector<int>&postorder,int postart,int postend, map<int,int>&mpp){
+        if(postart>postend||instart>inend) return NULL;
+        TreeNode*root=new TreeNode(postorder[postend]);
+        int inroot=mpp[postorder[postend]];
+        int numsleft=inroot-instart;
+        root->left=build(inorder,instart,inroot-1,postorder,postart,postart+numsleft-1,mpp);
+        root->right=build(inorder,inroot+1,inend,postorder,postart+numsleft,postend-1,mpp);
         return root;
     }
-
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        if (inorder.size() != postorder.size()) return NULL;
-        map<int, int> hm;
-        for (int i = 0; i < inorder.size(); i++) hm[inorder[i]] = i;
-        return PostIn(inorder, 0, inorder.size() - 1, postorder, 0, postorder.size() - 1, hm);
+        int n=inorder.size(), m=postorder.size();
+        if(n!=m) return NULL;
+        map<int,int>mpp;
+        for(int i=0;i<n;i++) mpp[inorder[i]]=i;
+        return build(inorder,0,n-1,postorder,0,m-1,mpp);
     }
 };
