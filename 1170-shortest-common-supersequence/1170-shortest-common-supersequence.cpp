3fb98afa1bea52be02bf355738bce7419afcb425
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-16 18:13:09 +0530

    Time: 25 ms (60.46%), Space: 26.6 MB (95.81%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -5,7 +5,7 @@ public:
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(str1[i-1]==str2[j-1]) dp[i][j]=max(dp[i][j],dp[i-1][j-1]+1);
+                if(str1[i-1]==str2[j-1]) dp[i][j]=dp[i-1][j-1]+1;
                 else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
         }
