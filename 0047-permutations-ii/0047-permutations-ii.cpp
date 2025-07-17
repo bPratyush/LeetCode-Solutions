863f0b7c776e238d0aa4a7ab2786b86fd9abce1a
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-17 12:57:09 +0530

    Time: 11 ms (30.9%), Space: 15.2 MB (19.85%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void bt(int idx,vector<vector<int>>&res,vector<int>&nums,unordered_set<int>&seenidx){
+    void bt(int idx,vector<vector<int>>&res,vector<int>&nums){
         int n=nums.size();
         if(idx==n){
             res.push_back(nums); return;
@@ -10,15 +10,14 @@ public:
             if(used.find(nums[i])!=used.end()) continue;
             used.insert(nums[i]);
             swap(nums[idx],nums[i]);
-            bt(idx+1,res,nums,seenidx);
+            bt(idx+1,res,nums);
             swap(nums[idx],nums[i]);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>>res;
-        unordered_set<int>seenidx;
         sort(nums.begin(),nums.end());
-        bt(0,res,nums,seenidx);
+        bt(0,res,nums);
         return res;
     }
 };
