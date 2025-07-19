93d10194de25ff86e59c6ce37ee5801c7ff1ed94
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-19 16:16:18 +0530

    Time: 7 ms (68.85%), Space: 11.5 MB (44.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int> mpp(128,0);
-        for(char c:t) mpp[c]++;
-        int counter=t.size(), b=0, e=0, d=INT_MAX, head=0;
+        unordered_map<char,int> freq;
+        for(char c:t) freq[c]++;
+        int counter=t.size(),b=0,e=0,d=INT_MAX,head=0;
         while(e<s.size()){
-            if(mpp[s[e++]]-->0) counter--;
+            if(freq[s[e++]]-- > 0) counter--;
             while(counter==0){
                 if(e-b<d){
                     head=b;
                     d=e-head;
                 }
-                if(mpp[s[b++]]++==0) counter++;
+                if(!freq[s[b++]]++) counter++;
             }
         }
         return d==INT_MAX?"":s.substr(head,d);
