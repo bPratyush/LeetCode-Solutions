056395b39d3a9b8990444e70024f977979e1f79a
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-19 11:02:41 +0530

    Time: 0 ms (100%), Space: 9.1 MB (66.18%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int lenS = s.length();
-        int lenP = p.length();
-        vector<vector<bool>> mat(lenS + 1, vector<bool>(lenP + 1, false));
-
-        mat[0][0] = true;
-
-        for (int i = 1; i <= lenP; i++) {
-            if (p[i - 1] == '*') {
-                mat[0][i] = mat[0][i - 2];
+        int slen=s.length();
+        int plen=p.length();
+        vector<vector<bool>> mat(slen+1,vector<bool>(plen+1,false));
+        mat[0][0]=true;
+        for(int i=1;i<=plen;i++){
+            if(p[i-1]=='*'){
+                mat[0][i]=mat[0][i-2];
             }
         }
-
-        for (int i = 1; i <= lenS; i++) {
-            for (int j = 1; j <= lenP; j++) {
-                if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
-                    mat[i][j] = mat[i - 1][j - 1];
-                } else if (p[j - 1] == '*') {
-                    mat[i][j] = mat[i][j - 2];
-                    if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
-                        mat[i][j] = mat[i][j] || mat[i - 1][j];
-                    }
-                } else {
-                    mat[i][j] = false;
+        for(int i=1;i<=slen;i++){
+            for(int j=1;j<=plen;j++){
+                if(p[j-1]=='.'||p[j-1]==s[i-1]) mat[i][j]=mat[i-1][j-1];
+                else if(p[j-1]=='*') {
+                    mat[i][j]=mat[i][j-2];
+                    if(p[j-2]=='.'||p[j-2]==s[i-1]) mat[i][j]=mat[i][j]||mat[i-1][j];
                 }
+                else mat[i][j]=false;
             }
         }
-
-        return mat[lenS][lenP];
+        return mat[slen][plen];
     }
 };
