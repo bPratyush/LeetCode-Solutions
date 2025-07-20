9650560404dc8336fa2116bb7da232240c3fab6a
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-20 22:40:13 +0530

    Time: 14 ms (37.35%), Space: 16.8 MB (38.71%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -5,10 +5,10 @@ public:
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                    if(word1[i-1]==word2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                    else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
+                if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1]+1;
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
+        }
         return n+m-2*dp[n][m];
     }
 };
