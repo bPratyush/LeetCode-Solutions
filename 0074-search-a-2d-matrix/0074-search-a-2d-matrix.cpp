ebf36789f8e17d54ecb71372512da35fe412666b
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-21 12:52:17 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.04%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,13 +4,12 @@ public:
         int rsize=matrix.size();
         int csize=matrix[0].size();
         int l=0,r=rsize*csize-1;
-        int mid=l+(r-l)/2;
         while(l<=r){
+            int mid=l+(r-l)/2;
             int elem=matrix[mid/csize][mid%csize];
             if(target==elem) return 1;
             else if(target<=elem) r=mid-1;
             else l=mid+1;
-            mid=l+(r-l)/2;
         }
         return 0;
     }
