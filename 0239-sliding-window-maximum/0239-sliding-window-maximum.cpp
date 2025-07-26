c93a17fc2c44435dbb33369866cf5492e15ee1e3
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-26 19:53:07 +0530

    Time: 28 ms (42.39%), Space: 139.2 MB (35.8%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,14 +5,10 @@ public:
         vector<int> ans;
         for(int i = 0 ; i < nums.size();i++){
             while(!dq.empty() && dq.front() <= i-k) dq.pop_front();
-
-            while(!dq.empty() && nums[dq.back()] < nums[i]){
-                dq.pop_back();
-            }
+            while(!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
             dq.push_back(i);
-            if(i-k+1 >= 0 )ans.push_back(nums[dq.front()]);
+            if(i-k+1 >= 0) ans.push_back(nums[dq.front()]);
         }
-
         return ans;
     }
 };
