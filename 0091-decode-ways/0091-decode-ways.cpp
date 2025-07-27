376a13726cfc847615fc61a0d3ed5c016f07ae3d
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-27 21:17:21 +0530

    Time: 0 ms (100%), Space: 8.6 MB (86.27%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -4,7 +4,8 @@ public:
         int n=s.size();
         vector<int>dp(n+1,0);
         dp[0]=1;
-        s[0]!='0'?dp[1]=1:dp[1]=0;
+        if(s[0]=='0') dp[1]=0;
+        else dp[1]=1;
         for(int i=2;i<=n;i++){
             if(s[i-1]!='0') dp[i]+=dp[i-1];
             if(s[i-2]=='1'||s[i-2]=='2'&&s[i-1]<='6') dp[i]+=dp[i-2];
