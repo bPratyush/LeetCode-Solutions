d8d4ab1c195d8f555c62f0ed9cb3f8223eb54d8d
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-07-27 22:20:54 +0530

    Time: 0 ms (100%), Space: 19.5 MB (74.72%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,45 +1,41 @@
-class disjointset {
-public:
-    vector<int> rank, parent;
-    disjointset(int V) {
-        rank.resize(V + 1, 0);
-        parent.resize(V + 1, 0);
-        for (int i = 0; i <= V; i++) parent[i] = i;
-        for (int i = 0; i <= V; i++) rank[i] = 1;
+class DSU{
+    public:
+    vector<int>rank,par;
+    DSU(int n){
+        rank.resize(n+1,1);
+        par.resize(n+1,0);
+        for(int i=0;i<=n;i++) par[i]=i;
     }
-    int findparent(int u) {
-        if (u == parent[u]) return u;
-        return parent[u] = findparent(parent[u]); //parent[u]=.. for Path Compression
+    int findpar(int u){
+        if(u==par[u]) return u;
+        return par[u]=findpar(par[u]);
     }
-    void unionbyRank(int u, int v) {
-        int ulp = findparent(u);
-        int ulv = findparent(v);
-        if (ulp == ulv) return;
-        if (rank[ulp] < rank[ulv]) parent[ulp] = ulv;
-        else if (rank[ulv] < rank[ulp]) parent[ulv] = ulp;
-        else {
-            parent[ulp] = ulv;
+    void unite(int u,int v){
+        int ulp=findpar(u);
+        int ulv=findpar(v);
+        if(ulp==ulv) return;
+        if(rank[ulp]<rank[ulv]) par[ulp]=ulv;
+        else if(rank[ulv]<rank[ulp]) par[ulv]=ulp;
+        else{
+            par[ulp]=ulv;
             rank[ulv]++;
         }
     }
 };
-
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int V = isConnected.size();
-        disjointset ds(V);
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
-                if (isConnected[i][j])
-                    ds.unionbyRank(i, j);
+        int n=isConnected.size();
+        DSU dsu(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]) dsu.unite(i,j);
             }
         }
-        int cnt = 0;
-        for (int i = 0; i < V; i++) {
-            if (ds.findparent(i) == i) cnt++;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(dsu.findpar(i)==i) cnt++;
         }
         return cnt;
     }
 };
-
