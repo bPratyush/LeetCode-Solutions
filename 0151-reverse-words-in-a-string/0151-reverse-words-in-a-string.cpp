06ca296b57feb7c36f3a85a29d6598da8dd0f20a
Pratyush Bindal <81455621+bPratyush@users.noreply.github.com>
2025-08-02 16:21:02 +0530

    Time: 0 ms (100%), Space: 10.2 MB (79.23%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string ans = "";
-        string word = "";
-        for (int i = 0; i < s.length(); ++i) {
-            if (s[i] == ' ') {
-                if (!word.empty()) {
-                    reverse(word.begin(), word.end());
-                    ans += word + " ";
-                    word = "";
+        string res="",word="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' '){
+                if(!word.empty()){
+                    reverse(word.begin(),word.end());
+                    res+=word+" ";
+                    word="";
                 }
-            } else {
-                word += s[i];
             }
+            else word+=s[i];
         }
-        if (!word.empty()) {
-            reverse(word.begin(), word.end());
-            ans += word;
-        }
-        if (!ans.empty() && ans.back() == ' ') ans.pop_back();
-        reverse(ans.begin(), ans.end());
-        return ans;
+        if(!word.empty()){
+            reverse(word.begin(),word.end());
+            res+=word;
+        }  
+        if(!res.empty()&&res.back()==' ') res.pop_back();
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
